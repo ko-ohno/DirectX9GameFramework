a79237a32611ac4ddc7275916762b9906b13e271
OhNo3 <41865015+OhNo3@users.noreply.github.com>
2020-12-16 22:46:12 +0900

    メインの処理を追加

---
+++ Project/Project/SourceFiles/DebugSystem.cpp
@@ -0,0 +1,38 @@
+/*=============================================================================
+/*-----------------------------------------------------------------------------
+/*	[DebugSystem.cpp] デバッグシステム
+/*	Author：Kousuke,Ohno.
+/*-----------------------------------------------------------------------------
+/*	説明：デバッグシステム
+=============================================================================*/
+
+/*--- インクルードファイル ---*/
+#include "StdAfx.h"
+#include "DebugSystem.h"
+
+/*-----------------------------------------------------------------------------
+/* デバッグ用の文字列データ出力処理
+-----------------------------------------------------------------------------*/
+std::string DebugSystem::OutputErrorString(std::string inErrorMsg, std::string inFileName, int inLineNumber, std::string inFunctionName)
+{
+	//ファイル名をだけを取り出す
+	std::string file_name = inFileName;
+	{
+		int str_erase_begin = 0;							//ディレクトリの0文字目
+		int str_erase_end = file_name.find_last_of("\\");	//一番最後のディレクトリを検索
+
+		//不要な文字列の削除処理
+		file_name.erase(str_erase_begin, str_erase_end + 1);
+	}
+
+	//各、文字列の要素をまとめたもの
+	std::string file_name_and_line_num  = "ファイル名：" + file_name      + "(" + std::to_string(inLineNumber) + ")\n";
+	std::string function_name			= "関数名    ：" + inFunctionName + "()\n";
+	std::string error_msg				= "エラー内容：" + inErrorMsg     + "\n";
+
+	return std::string(file_name_and_line_num + function_name + error_msg);
+}
+
+/*=============================================================================
+/*		End of File
+=============================================================================*/
---
+++ Project/Project/SourceFiles/Main.cpp
@@ -0,0 +1,52 @@
+﻿/*=============================================================================
+/*-----------------------------------------------------------------------------
+/*	[Main.cpp] メインモジュール
+/*	Author：Kousuke,Ohno.
+/*-----------------------------------------------------------------------------
+/*	説明：メインで実行する処理の流れを定義
+=============================================================================*/
+
+/*--- インクルードファイル ---*/
+#include "StdAfx.h"
+#include "MainProcess.h"
+
+/*-----------------------------------------------------------------------------
+/* メイン処理
+-----------------------------------------------------------------------------*/
+//ビルドの設定によってコンソール画面を表示するかを決定する
+
+#if defined(_DEBUG) || defined(DEBUG)
+int main(void)
+{
+#else
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+#endif
+
+	//メモリリーク検知用
+#if defined(_DEBUG) || defined(DEBUG) //下記の関数はリリースビルド時に実行されないらしいけれど、一応設定しておく
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+	MainProcess* main_process_ = NEW MainProcess();
+
+	/*----------------------------------------------
+	/* メインの実行命令
+	----------------------------------------------*/
+	{
+		MainProcess main_process;
+
+		bool is_success = main_process.StartUp();	//起動処理
+		if (is_success)
+		{
+			main_process.Run();						//実行処理
+		}
+		main_process.ShutDown();					//停止処理
+	}
+	return 0;
+}
+
+/*=============================================================================
+/*		End of File
+=============================================================================*/
