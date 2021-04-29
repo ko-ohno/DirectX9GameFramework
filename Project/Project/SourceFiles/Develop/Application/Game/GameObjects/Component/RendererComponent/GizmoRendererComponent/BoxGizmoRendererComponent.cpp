dbd078d4af958a9aba8d1c123a66976f9f7f29ad
OhNo3 <41865015+OhNo3@users.noreply.github.com>
2021-04-30 02:49:53 +0900

    ギズモ描画コンポーネントの作成

---
+++ Project/Project/SourceFiles/Develop/Application/Game/GameObjects/Component/RendererComponent/GizmoRendererComponent/BoxGizmoRendererComponent.cpp
@@ -0,0 +1,1 @@
+#include "BoxGizmoRendererComponent.h"
--- Project/Project/SourceFiles/Develop/Application/Game/GameObjects/Component/RendererComponent/GizmoRendererComponent/GridGizmoRendererComponent.h
+++ Project/Project/SourceFiles/Develop/Application/Game/GameObjects/Component/RendererComponent/GizmoRendererComponent/GridGizmoRendererComponent.h
@@ -1,14 +1,47 @@
-#pragma once
-class GridRendererComponent
+/*=============================================================================
+/*-----------------------------------------------------------------------------
+/*	[GridRendererComponent.h]  グリッドギズモ描画コンポーネント
+/*	Author：Kousuke,Ohno.
+/*-----------------------------------------------------------------------------
+/*	説明：グリッドギズモ描画コンポーネント
+=============================================================================*/
+#ifndef GRID_GIZMO_RENDERER_COMPONENT_H_
+#define	GRID_GIZMO_RENDERER_COMPONENT_H_
+
+/*--- インクルードファイル ---*/
+#include "../GizmoRendererComponent.h"
+
+/*--- 構造体定義 ---*/
+
+/*--- クラスの前方宣言 ---*/
+
+
+/*-------------------------------------
+/* グリッドギズモ描画コンポーネント
+-------------------------------------*/
+class GridRendererComponent : public GizmoRendererComponent
 {
 public:
-	GridRendererComponent(void);
+	GridRendererComponent(class GameObject* owner, int updateOrder = 100);
 	~GridRendererComponent(void);
+
+private:
+	bool Init(void) override;
+	void Uninit(void) override;
+
+public:
+	void Input(void) override;
+	void Update(float deltaTime) override;
+
+	virtual TypeID GetComponentType() const override { return TypeID::GridRendererComponent; };
 
 	void WireFlameON(void);
 	void WireFlameOFF(void);
 
-
 private:
 };
 
+#endif //GRID_GIZMO_RENDERER_COMPONENT_H_
+/*=============================================================================
+/*		End of File
+=============================================================================*/
